ba67d8a5b7d0a72727ef797194dd9bfc42070808
piht <piht@pihtdeMac-mini.local>
2014-10-20 09:51:07 +0800

    Initial Commit

---
+++ Browser/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Browser
+//
+//  Created by piht on 14-10-20.
+//  Copyright (c) 2014年 piht. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Browser/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Browser
+//
+//  Created by piht on 14-10-20.
+//  Copyright (c) 2014年 piht. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
